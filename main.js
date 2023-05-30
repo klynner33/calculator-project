2eef099425b2991841197720b92dce268f5b0f4f
klynner33 <113389068+klynner33@users.noreply.github.com>
2023-05-30 16:09:48 -0500

    Set up calculator layout and buttons with characters

---
+++ main.js
@@ -0,0 +1,15 @@
+let sum = (n1, n2) => console.log(n1 + n2)
+let difference = (n1, n2) => console.log(n1 - n2)
+let product = (n1, n2) => console.log(n1 * n2)
+let quotient = (n1, n2) => console.log(n1 / n2)
+
+sum(1, 3)
+difference(5, 2)
+product(2, 3)
+quotient(6, 3)
+
+let firstNum = 0
+let operator = +
+let secondNum = 1
+
+const operate = (operator, num1, num2) => sum()
